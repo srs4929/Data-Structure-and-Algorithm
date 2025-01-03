ee787e287a4ec642972c0a8623cd97b7394573f4
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-01-03 09:53:25 +0600

    Add files via upload

---
+++ Lab/RecursionLab/07_A.cpp
@@ -0,0 +1,38 @@
+//07
+//07
+#include<bits/stdc++.h>
+using namespace std;
+
+void com(vector<int>&arr,int index,int k,vector<int>result)
+{
+      if(result.size()==k)
+      {
+           for(int i=0;i<result.size();i++)
+           {
+              cout<<result[i]<<" ";
+           }
+           cout<<endl;
+           return;
+      }
+      if(index>=arr.size()) return;
+
+      result.push_back(arr[index]);
+      com(arr,index+1,k,result);
+      result.pop_back();
+      com(arr,index+1,k,result);
+}
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=i;
+    }
+    sort(arr.begin(),arr.end(),greater<int>());
+    vector<int>result;
+    com(arr,0,k,result);
+    
+
+}
---
+++ Lab/RecursionLab/07_B.cpp
@@ -0,0 +1,29 @@
+//07
+#include<bits/stdc++.h>
+using namespace std;
+void print(int arr[],int k,int index)
+{
+    if(index==k)
+    {
+        for(int i=0;i<k;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    for(int i=4;i>=0;i--)
+    {
+        if((index+1)%2!=i%2) continue;
+        arr[index]=i;
+        print(arr,k,index+1);
+    }
+}
+int main()
+{
+    int k;
+    cin>>k;
+    int arr[k+1];
+    print(arr,k,0);
+
+}
