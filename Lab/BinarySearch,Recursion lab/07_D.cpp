b6ca1bbdba907d5931ff49011115e6c1287c891a
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-01-04 16:23:34 +0600

    Add files via upload

---
+++ Lab/BinarySearch,Recursion lab/07_D.cpp
@@ -0,0 +1,44 @@
+//07
+/*First line: n, a number (1<=n<=10).
+Output:
+Each line will contain a list of space-separated digits where
+● The list contains n digits.
+● The digits in the list are 0-4 (inclusive)
+● The odd indexed digit in the list is an odd digit
+● The even indexed digit in the list is an even digit
+The lists will be printed in sorted order. Example: {0, 1, 0} before {0, 1, 2}.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void print(int arr[],int n,int index)
+{
+    if(index==n)
+    {
+        for(int i=0;i<n;i++)
+        {
+           if(i==n-1)
+           {
+            cout<<arr[i];
+           }
+           else{
+            cout<<arr[i]<<" ";
+           }
+        }
+        cout<<endl;
+        return;
+    }
+    for(int i=0;i<5;i++)
+    {
+        if(i%2!=index%2) continue;
+        arr[index]=i;
+        print(arr,n,index+1);
+        
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    print(arr,n,0);
+}
