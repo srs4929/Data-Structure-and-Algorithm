6f3d51229ea1a7b8e017d7665330e7a6bda341bd
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-01-24 16:58:18 +0600

    Add files via upload

---
+++ Lab/lab final (28batch)/07_D.cpp
@@ -0,0 +1,35 @@
+//07
+#include<bits/stdc++.h>
+using namespace std;
+void permute(int arr[],int n,int index)
+{
+    if(index>=n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(i==n-1) cout<<arr[i];
+            else cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    for(int i=0;i<5;i++)
+    {
+        if(index%2!=i%2) continue;
+        arr[index]=i;
+        permute(arr,n,index+1);
+    }
+}
+
+
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    permute(arr,n,0);
+
+}
