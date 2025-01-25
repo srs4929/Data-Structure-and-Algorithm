a3604d22f2d33710ace5d183c3ead5117ffc5804
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-01-25 19:29:56 +0600

    Add files via upload

---
+++ Lab/heap,binarysearchlab/07_B.cpp
@@ -0,0 +1,69 @@
+//07
+#include<bits/stdc++.h>
+using namespace std;
+int heap[10000];
+int last=-1;
+
+int parent(int i)
+{
+    return (i-1)/2;
+}
+void heapup(int i)
+{
+    while(i>0)
+    {   
+        int p=parent(i);
+        if(heap[i]>heap[p]) break;
+        swap(heap[p],heap[i]);
+        i=p;
+    }
+}
+void insert(int x)
+{
+    heap[last+1]=x;
+    last++;
+    heapup(last);
+}
+
+void heapdown (int i)
+{
+   while(i<=last)
+   {
+       int left=2*i+1;
+       int right=2*i+2;
+       int small=i;
+       if(left<=last&&heap[left]<heap[small]) small=left;
+       if(right<=last&& heap[right]<heap[small]) small=right;
+       if(small!=i)
+       {
+        swap(heap[small],heap[i]);
+        i=small;
+       }
+       else break;
+   }
+    
+}
+int extractmin()
+{
+    int mini=heap[0];
+    heap[0]=heap[last];
+    last--;
+    heapdown(0);
+    return mini;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        insert(x);
+        
+    }
+    while(last>=0)
+    {
+        cout<<extractmin()<<endl;
+    }
+}
---
+++ Lab/heap,binarysearchlab/A.cpp
@@ -0,0 +1,33 @@
+//07
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(int arr[],int s,int e,int t)
+{
+    while(s<=e)
+    {
+        int mid=(s+e)/2;
+        if(arr[mid]==t) return 1;
+        else if(arr[mid]>t) e=mid-1;
+        else s=mid+1;
+    }
+    return 0;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    sort(arr,arr+n);
+    int m;
+    cin>>m;
+    for(int i=0;i<m;i++)
+    {
+        int q;
+        cin>>q;
+        cout<<search(arr,0,n-1,q)<<endl;
+
+    }
+}
